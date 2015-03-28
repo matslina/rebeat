6ab85dfb1d76677ddbd51eb0c8c0870a44a9d1b0
Mats Linander <matslina@gmail.com>
2015-03-28 14:56:23 -0400

    break out audio cruft and use player object instead
    also got rid of redundant matplotlib imports

--- rebeat.py
+++ rebeat.py
@@ -2,31 +2,15 @@
 # -*- coding: utf-8 -*-
 
 import Tkinter as tk
-
-
-import matplotlib
-matplotlib.use("TkAgg")
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
-from matplotlib.figure import Figure
-
-import wave
-import struct
-
 import audioselector
 import beatgrid
-
+import player
 
 import sys
 if len(sys.argv) < 2:
     sys.stderr.write("usage: %s <audiofile>\n" % sys.argv[0])
     sys.exit(1)
 
-w = wave.open(sys.argv[1])
-assert w.getsampwidth() == 2
-raw = w.readframes(w.getnframes())
-frames = [struct.unpack("<h", raw[i:i+2])[0] / float(2**15)
-          for i in range(0, len(raw), 2)]
-fps = w.getframerate()*2
 
 class ReBeatApp(tk.Tk):
     def __init__(self):
@@ -34,10 +18,14 @@ class ReBeatApp(tk.Tk):
         self.configure(background='white')
         self.grid()
 
+        self.player = player.Player(sys.argv[1])
+
         # UI components from top to bottom
 
         # Audio visualization and selection
-        self.audio = audioselector.AudioSelector(self, frames, fps,
+        self.audio = audioselector.AudioSelector(self,
+                                                 self.player.get_signal(),
+                                                 self.player.get_fps(),
                                                  highlightthickness=0)
         self.audio.grid(column=0, row=0, columnspan=2)
         self.audio.on_create_selection(self.selection_created)
@@ -60,11 +48,6 @@ class ReBeatApp(tk.Tk):
         self.resolution = tk.Spinbox(self, values=(1,2,4,8,16))
         self.bars.grid(row=2, column=1)
 
-        self.marks = []
-
-        import pyaudio
-        self.p = pyaudio.PyAudio()
-
     def mark_created(self, i, time):
         self.beats.add_row()
 
@@ -72,27 +55,8 @@ class ReBeatApp(tk.Tk):
         print "selection", start, end
         print "all", self.audio.get_selections()
 
-    def play(self):
-        stream = self.p.open(format=self.p.get_format_from_width(2),
-                             channels=2,
-                             rate=fps/2,
-                             output=True)
-        samples = []
-        start = 0
-        for mark in self.marks + [len(frames)]:
-            end = int(mark * fps)*2
-            stream.write(raw[start:end])
-            stream.write(raw[start:end])
-            stream.write(raw[start:end])
-            stream.write(raw[start:end])
-            stream.write(raw[start:end])
-            start = end
-        stream.stop_stream()
-        stream.close()
-
 if __name__ == "__main__":
     app = ReBeatApp()
     app.title("ReBeat")
-    print len(frames)
     app.mainloop()
     #app.destroy()
